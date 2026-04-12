9619e6bd8b94496088d0d591fb183feadc7ab602
Christian Stamati <christian.stamati@gmail.com>
2026-04-12 22:42:19 +0200

    remove print

--- src/routes/_authenticated/tasks.tsx
+++ src/routes/_authenticated/tasks.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { UserInfo } from "@/components/UserInfo";
-import { PrintTasks } from "@/features/tasks/PrintTasks";
 import { TaskList } from "@/features/tasks/TaskList";
 
 export const Route = createFileRoute("/_authenticated/tasks")({
@@ -12,7 +11,6 @@ function App() {
 		<div className="mx-auto flex w-full max-w-md flex-col gap-4 pt-4">
 			<UserInfo />
 			<TaskList />
-			<PrintTasks />
 		</div>
 	);
 }
